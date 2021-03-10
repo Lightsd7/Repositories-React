c38ead72685cbd36be3bb3e601659e58f6b8b36d
Raphael Molina Bedran <tqi_rbedran@uolinc.com>
2021-03-10 09:05:12 -0300

    Implement search

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import React, { useState, useEffect } from "react";
+import { FiCheckSquare } from "react-icons/fi";
 
 interface Repository {
   name: string;
@@ -13,20 +14,59 @@ interface Repository {
 
 export function RepositoryList() {
   const [repositories, setRapositories] = useState<Repository[]>([]);
+  const [newRepository, setnewRepository] = useState("");
+  const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch("https://api.github.com/orgs/abr3dev/repos")
-      .then((response) => response.json())
-      .then((data) => setRapositories(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/orgs/abr3dev/repos")
+  //     .then((response) => response.json())
+  //     .then((data) => setRapositories(data));
+  // }, []);
+
+  function handleCreatenewRepository() {
+    setMessage("");
+    newRepository
+    ? fetch(`https://api.github.com/orgs/${newRepository}/repos`)
+        .then((response) => response.json())
+        .then((data) => setRapositories(data))
+    : setMessage("Repositório não existe.");
+  }
+
+  function validateRepository() {}
 
   return (
     <section className="repository-list">
-      <h1>Lista de Repositórios da <a href="https://github.com/orgs/Abr3Dev">Abr3Dev</a></h1>
+      <header>
+        { newRepository
+          ? <h1>Lista de Repositórios da <a href={`https://github.com/orgs/${newRepository}`}>{ newRepository }</a></h1>
+          : <h1>Pesquise a organização no <span>GitHub</span></h1> }
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Pesquisar repositório"
+            onChange={(e) => setnewRepository(e.target.value)}
+            value={newRepository}
+          />
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreatenewRepository}
+          >
+            <FiCheckSquare size={16} />
+          </button>
+          { message
+            ? <p>{ message }</p>
+            : null
+          }
+        </div>
+      </header>
       <ul>
-        {repositories.map((repository) => {
-          return <RepositoryItem key={repository.name} repository={repository} />;
-        })}
+        { repositories.length > 0
+          ? repositories.map((repository) => {
+            return <RepositoryItem key={repository.name} repository={repository} />;
+          })
+          : repositories.length <= 0 ? null : <p>Organização não encontrada</p>
+        }
       </ul>
     </section>
   );
